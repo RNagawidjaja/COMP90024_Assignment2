54e120e58b4e37119299bb9d94c6dcfd31e030e7
rnagawidjaja <nagawidjajar@student.unimelb.edu.au>
2019-04-20 16:52:32 +1000

    Initial twitter crawler

---
+++ twitter_crawler/twitter_crawler.py
@@ -0,0 +1,115 @@
+# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
+from tweepy import API
+from tweepy import Cursor #use for iterating through multiple tweets
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream  #streaming API of twittter
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import re
+ 
+import twitter_credentials #file containing Consumer and API keys
+
+# TWITTER CLIENT
+
+class TwitterClient():
+    def __init__(self, twitter_user=None):
+        self.auth = TwitterAuthenticator().authenticate_twitter_app()
+        self.twitter_client = API(self.auth)
+        
+        self.twitter_user = twitter_user
+        
+    def get_twitter_client_api(self):
+        return self.twitter_client
+        
+    def get_user_timeline_tweets(self, num_tweets):
+        tweets = []
+        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets): #twitter_client is a internal variable of Cursor
+            tweets.append(tweet)
+        return tweets
+            
+        
+    def get_friend_list(self, num_friends):
+        friend_list = []
+        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
+            friend_list.append(friend)
+        return friend_list
+        
+    def get_home_timeline_tweets(self, num_tweets):
+        home_timeline = []
+        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(self.num_tweets):
+            home_timeline.append(tweet)
+        return home_timeline
+        
+        
+    def search_tweet(self, params):
+        tweets = []
+        
+        for tweet in Cursor(self.twitter_client.search, q=params).items(5):
+            tweets.append(tweet)
+        return tweet
+        
+# # # # TWITTER AUTHENTICATOR # # #
+
+class TwitterAuthenticator():
+    def authenticate_twitter_app(self):
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+        return auth
+ 
+# # # # TWITTER STREAMER # # # #
+class TwitterStreamer():
+    """
+    Class for streaming and processing live tweets.
+    """
+    def __init__(self):
+        self.twitter_authenticator = TwitterAuthenticator()
+
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+        # This handles Twitter authetification and the connection to Twitter Streaming API
+        auth = self.twitter_authenticator.authenticate_twitter_app()
+        listener = TwitterListener(fetched_tweets_filename)
+        stream = Stream(auth, listener)
+
+        # This line filter Twitter Streams to capture data by the keywords: 
+        stream.filter(track=hash_tag_list)
+
+
+# # # # TWITTER STREAM LISTENER # # # #
+class TwitterListener(StreamListener):
+    """
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def __init__(self, fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
+
+    def on_data(self, data):
+        try:
+            print(data)
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data.rstrip('\n'))
+                #tf.write(data.rstrip('\n')) #for windwows??
+            return True
+        except BaseException as e:
+            print("Error on_data %s" % str(e))
+        return True
+          
+
+    def on_error(self, status):
+        if status == 420:
+            
+            #Returning false on_data method in case rate limit occurs
+            return False
+        print(status)
+
+ 
+if __name__ == '__main__':
+    hash_tag_list = ['nba']
+    fetched_tweets_filename = 'tweets.json'
+    twitter_streamer = TwitterStreamer()
+    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
+    #twitter_client = TwitterClient()
+    #print(twitter_client.search_tweet('Sixers'))
