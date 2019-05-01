848921f724d65e02d95a348ce907542bf836c86f
rnagawidjaja <nagawidjajar@student.unimelb.edu.au>
2019-05-01 11:56:31 +1000

    make the polygons and multipolygons of the lga in the constructor

--- LGA_filter/lga_filter.py
+++ LGA_filter/lga_filter.py
@@ -8,62 +8,67 @@ import sys
 
 class LGA_filter:
     def __init__(self, lga_file):
+        self._polygons = []
+        #self._multi_polygon = []
         with open(LGA_file, encoding='UTF-8') as lf:
             self.lga = json.load(lf)
-            
-    def filter(self, point):
-        #point = tweet['value']['coordinates']
-        point = [point[1], point[0]]
-        point = Point(tuple(point))
-        lga_name = None
         for row in self.lga['rows']:
-            coordinates = row['doc']['geometry']['coordinates']
-            type = row['doc']['geometry']['type']
+            polygon = []
             polygons = []
+            type = row['doc']['geometry']['type']
+            coordinates = row['doc']['geometry']['coordinates']
+            id = row['id']
             if type == 'MultiPolygon':
                 for c in coordinates:
                     polygon = [tuple(l) for l in c[0]]
                     polygon = Polygon(polygon)
                     polygons.append(polygon)
                 multi_polygon = MultiPolygon(polygons)
-                if multi_polygon.contains(point):
-                    lga_name = row['doc']['lga_name']
-                    break
-                else:
-                    continue
+                self._polygons.append([id, multi_polygon])
             elif type == 'Polygon':
                 polygon = [tuple(l) for l in coordinates[0]]
                 polygon = Polygon(polygon)
-                if polygon.contains(point):
-                    lga_name = row['doc']['lga_name']
-                    break
-                else:
-                    continue
+                self._polygons.append([id, polygon])
             else:
-                print('Incorrect type')
-        return lga_name
+                print('Incorrect type')    
+            
+    def filter(self, point):
+        #point = tweet['value']['coordinates']
+        point = [point[1], point[0]]
+        point = Point(tuple(point))
+        
+        for p in self._polygons:
+            lga_id = None
+            if p[1].contains(point):
+                lga_id = p[0]
+                break
+            else:
+                continue
+        return lga_id
             
 
 if __name__ == '__main__':
-    tweets_file = r'C:\Users\reyna\Documents\Unimelb\COMP90024\Assignment_2\tweets\tweets_coordinates.json'
+    #for testing
+    #tweets_file = r'C:\Users\reyna\Documents\Unimelb\COMP90024\Assignment_2\tweets\tweets_coordinates.json'
     LGA_file = r'C:\Users\reyna\Documents\Unimelb\COMP90024\Assignment_2\tweets\LGA.json'
     
-    with open(tweets_file, encoding='UTF-8') as tf:
-        tweets = json.load(tf)
+    #with open(tweets_file, encoding='UTF-8') as tf:
+    #    tweets = json.load(tf)
     #y = tweets['rows'][0]['value']['coordinates']
     
     couchserver = couchdb.Server('http://45.113.233.19:8081')
     db_tweets = couchserver['tweets']
     db_tweets_geo = couchserver['tweets_geo']
-    lga_f = LGA_filter(LGA_file)
+    lga = LGA_filter(LGA_file)
     for item in db_tweets.view('_design/geo/_view/geo', descending = True):
-        lga_name = lga_f.filter(item.value['coordinates'])
-        doc = {'_id': item.id, 'coordinates': item.value['coordinates'], 'lga_name' : lga_name}
-        try:
-            db_tweets_geo.save(doc)
-        except couchdb.http.ResourceConflict:
-            print(item.id)
-            print('ID has already existed')
-            sys.exit()
+        lga_id = lga.filter(item.value['coordinates'])
+        print(lga_id)
+        #doc = {'_id': item.id, 'coordinates': item.value['coordinates'], 'lga_name' : lga_name}
+        #try:
+        #    db_tweets_geo.save(doc)
+        #except couchdb.http.ResourceConflict:
+        #    print(item.id)
+        #    print('ID has already existed')
+        #    sys.exit()
     
     #print(list(map(lga_f.filter, tweets['rows'])))
