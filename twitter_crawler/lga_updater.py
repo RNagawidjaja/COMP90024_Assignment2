e8f310dcbb9ced9206aaad99473b602f55af7089
Michael Lumley <mlumley@student.unimelb.edu.au>
2019-05-02 20:52:37 +1000

    Added lga updater

---
+++ twitter_crawler/lga_updater.py
@@ -0,0 +1,21 @@
+from lga_filter import LGA_Filter
+
+import json
+import couchdb
+
+server = couchdb.Server('http://45.113.233.237:5984')
+tweets = server["tweets"]
+geojson = server["geojson"]
+
+db_geojson_view = geojson.view("_design/geojsonview/_view/geojsonview")
+lga = LGA_Filter(db_geojson_view)
+count = 0
+
+for row in tweets.view("_design/noLGAField/_view/noLGAField"):
+    doc = tweets.get(row["id"])
+    coordinates = row["value"]["coordinates"]
+    lga_id = lga.filter(coordinates)
+    doc["lga_id"] = lga_id
+    tweets.save(doc)
+    print(count)
+    count += 1
