4da0cb09cc70a8f24ae32d9df2e99769d7358c1d
Michael Lumley <mlumley@student.unimelb.edu.au>
2019-05-15 00:25:22 +1000

    Added db host

--- ansible/deploy_harvester.sh
+++ ansible/deploy_harvester.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 . ./unimelb-comp90024-group-77-openrc.sh
-ansible-playbook --key-file="~/.ssh/id_team77" deployHarvester.yml
+ansible-playbook --key-file="~/.ssh/id_team77" -i hosts deployHarvester.yml
